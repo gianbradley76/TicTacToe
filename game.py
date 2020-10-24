7255e4fad46e1e248048cadfe180fd75e2761e6f
Gian Bradley Mendoza <gbpmendoza76@gmail.com>
2020-10-24 15:08:07 +0800

    Fixed and finished player inputs

--- game.py
+++ game.py
@@ -1,8 +1,43 @@
+def change_player(current_player):
+    current_player = 'O' if current_player == 'X' else 'X'
+
+    def enter_coordinates(final_array, current_player):
+    while True:
+
+        coordinate_input = input('Enter the coordinates: ').split()
+        # table coordinates
+        # (1, 3)(2, 3)(3, 3)
+        # (1, 2)(2, 2)(3, 2)
+        # (1, 1)(2, 1)(3, 1)
+
+        # Check if input is integer values
+        try:
+            coordinate = [int(x) - 1 for x in coordinate_input[::-1]]
+            # Check if input is from 1 to 3
+            valid = [True if int(x) in range(1, 4)
+                     else False for x in coordinate_input]
+            if not all(valid):
+                print('Coordinates should be from 1 to 3!')
+            else:
+                # True = coordinate is not yet taken
+                # False = coordinate is not yet taken
+                if final_array[int(coordinate[0])][int(coordinate[1])][1] is True:
+                    final_array[int(coordinate[0])][int(
+                        coordinate[1])][1] = False
+                    final_array[int(coordinate[0])][int(
+                        coordinate[1])][0] = current_player
+                    break
+                else:
+                    print("This cell is occupied! Choose another one!")
+        except ValueError:
+            print('You should enter numbers!')
+
+
 def print_table(final_array):
     print(f'---------\n'
-          f'| {final_array[0][0][0]} {final_array[0][1][0]} {final_array[0][2][0]} |\n'
+          f'| {final_array[2][0][0]} {final_array[0][1][0]} {final_array[2][2][0]} |\n'
           f'| {final_array[1][0][0]} {final_array[1][1][0]} {final_array[1][2][0]} |\n'
-          f'| {final_array[2][0][0]} {final_array[2][1][0]} {final_array[2][2][0]} |\n'
+          f'| {final_array[0][0][0]} {final_array[2][1][0]} {final_array[0][2][0]} |\n'
           f'---------')
 
 
@@ -14,13 +49,6 @@ if __name__ == "__main__":
     # Make the input into an array
     initial_array = [char for char in initial_input]
 
-    # table coordinates
-    # (1, 3)(2, 3)(3, 3)
-    # (1, 2)(2, 2)(3, 2)
-    # (1, 1)(2, 1)(3, 1)
-    # [[[X, False], [X, False], [X, False]],
-    #  [[X, False], [X, False], [X, False]],
-    #  [[X, False, ][X, False], [X, False]]]
     # Declare final array for the start of the game
     final_array = []
 
@@ -46,7 +74,7 @@ if __name__ == "__main__":
         inner_container.append(True if char == ' ' else False)
 
         # print(index + 1)
-        #print(f'inner -> {inner_container}')
+        # print(f'inner -> {inner_container}')
 
         outer_container.append(inner_container)
         outer += 1
@@ -58,27 +86,21 @@ if __name__ == "__main__":
             outer = 0
             final_array.append(outer_container)
             outer_container = []
-            # print(f'!: Final -> {final_array}')
 
-    # 3
-    current_player = 'X' if num_X == num_O else 'X'
-    #####################
+    final_array = final_array[::-1]
+
+    # print(final_array)
+    current_player = 'X' if num_X == num_O else 'O'
 
     # Print the table in the start of the games
     print_table(final_array)
 
-    print(final_array)
+    # Input coordinates
+    enter_coordinates(final_array, current_player)
 
-    coordinate = input('Enter the coordinates: ').split()
-    coordinate = [int(x) - 1 for x in coordinate]
-    # True = coordinate is not yet taken
-    # False = coordinate is not yet taken
-    if final_array[int(coordinate[0])][int(coordinate[1])][1] is True:
-        final_array[int(coordinate[0])][int(coordinate[1])][1] = False
-        final_array[int(coordinate[0])][int(coordinate[1])][0] = current_player
-    else:
-        print("This cell is occupied! Choose another one!")
+    # Change current player
+    change_player(current_player)
 
-    print(final_array)
-
+    # print(final_array)
     print_table(final_array)
+    print(current_player)
