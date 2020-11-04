eb302a808f0bc659e3f726adad36b4a5f1462cb2
Gian Bradley Mendoza <gbpmendoza76@gmail.com>
2020-11-05 01:01:31 +0800

    no change

--- game.py
+++ game.py
@@ -2,26 +2,40 @@ import random
 import sys
 
 
-def comp_coordinate(final_array, current_player):
+def comp_coordinate(final_list, current_player):
+    """Generates a coordinate for computer
+
+    Args:
+        final_list (list): list of available coordinates
+        current_player (string): the current player
+    """
+
     print('Making move level "easy"')
     current_player = 'O'
     while True:
         comp_x = random.randint(0, 2)
         comp_y = random.randint(0, 2)
-        if final_array[comp_x][comp_y][1] is False:
-            final_array[comp_x][comp_y][1] = True
-            final_array[comp_x][comp_y][0] = current_player
+        if final_list[comp_x][comp_y][1] is False:
+            final_list[comp_x][comp_y][1] = True
+            final_list[comp_x][comp_y][0] = current_player
             break
 
 
 # Input coordinates
-def enter_coordinates(final_array):
+def enter_coordinates(final_list):
+    """Gets and checks user coordinate input
+    Valid table coordinates
+    (1, 3)(2, 3)(3, 3)
+    (1, 2)(2, 2)(3, 2)
+    (1, 1)(2, 1)(3, 1)
+
+    Args:
+        final_list (list ): list of availble coordinates
+    """
+
     current_player = 'X'
     while True:
-        # table coordinates
-        # (1, 3)(2, 3)(3, 3)
-        # (1, 2)(2, 2)(3, 2)
-        # (1, 1)(2, 1)(3, 1)
+        
         coordinate_input = input('Enter the coordinates: ').split()
         # Check if input is integer values
         try:
@@ -34,10 +48,10 @@ def enter_coordinates(final_array):
             else:
                 # True = coordinate is taken
                 # False = coordinate is not yet taken
-                if final_array[int(coordinate[0])][int(coordinate[1])][1] is False:
-                    final_array[int(coordinate[0])][int(
+                if final_list[int(coordinate[0])][int(coordinate[1])][1] is False:
+                    final_list[int(coordinate[0])][int(
                         coordinate[1])][1] = True
-                    final_array[int(coordinate[0])][int(
+                    final_list[int(coordinate[0])][int(
                         coordinate[1])][0] = current_player
                     break
                 else:
@@ -47,36 +61,40 @@ def enter_coordinates(final_array):
 
 
 # Check the state of the game
-def game_state(final_array, current_player):
+def game_state(final_list, current_player):
+    """Checks if the game is still ongoing, won, or draw
+
+    Args:
+        final_list (list): list of available coordinates
+        current_player (string): current player
+    """
+
     # Wnning Coordinates
     # Horizontal
-    H_1 = [final_array[2][0], final_array[2][1], final_array[2][2]]
-    H_2 = [final_array[1][0], final_array[1][1], final_array[1][2]]
-    H_3 = [final_array[0][0], final_array[0][1], final_array[0][2]]
+    H_1 = [final_list[2][0], final_list[2][1], final_list[2][2]]
+    H_2 = [final_list[1][0], final_list[1][1], final_list[1][2]]
+    H_3 = [final_list[0][0], final_list[0][1], final_list[0][2]]
 
     # Vertical
-    V_1 = [final_array[2][0], final_array[1][0], final_array[0][0]]
-    V_2 = [final_array[2][1], final_array[1][1], final_array[0][1]]
-    V_3 = [final_array[2][2], final_array[1][2], final_array[0][2]]
+    V_1 = [final_list[2][0], final_list[1][0], final_list[0][0]]
+    V_2 = [final_list[2][1], final_list[1][1], final_list[0][1]]
+    V_3 = [final_list[2][2], final_list[1][2], final_list[0][2]]
 
     # Diagonal
-    D_1 = [final_array[2][0], final_array[1][1], final_array[0][2]]
-    D_2 = [final_array[0][0], final_array[1][1], final_array[2][2]]
+    D_1 = [final_list[2][0], final_list[1][1], final_list[0][2]]
+    D_2 = [final_list[0][0], final_list[1][1], final_list[2][2]]
 
     winning_conditions = [H_1, H_2, H_3,
                           V_1, V_2, V_3,
                           D_1, D_2]
 
-    game_won = False
     for condition in winning_conditions:
         if condition[0][0] == condition[1][0] == condition[2][0] and condition[0][0] != ' ' and condition[1][0] != ' ' and condition[2][0] != ' ':
             print(f'{current_player} wins')
-            game_won = True
             sys.exit(0)
             break
 
     # Print the initial table -> end of the games
-
     is_draw = True
     all_occupied = True
     for condition in winning_conditions:
@@ -95,19 +113,29 @@ def game_state(final_array, current_player):
         print('Game not finished')
 
 
-# Print the table
-def print_table(final_array):
+def print_table(final_list):
+    """Prints out the table
+
+    ---------
+    | _ _ _ |
+    | _ _ _ |
+    | _ _ _ |
+    ---------
+
+    Args:
+        final_list (list): list of available coordinates
+    """
     print(f'---------\n'
-          f'| {final_array[2][0][0]} {final_array[2][1][0]} {final_array[2][2][0]} |\n'
-          f'| {final_array[1][0][0]} {final_array[1][1][0]} {final_array[1][2][0]} |\n'
-          f'| {final_array[0][0][0]} {final_array[0][1][0]} {final_array[0][2][0]} |\n'
+          f'| {final_list[2][0][0]} {final_list[2][1][0]} {final_list[2][2][0]} |\n'
+          f'| {final_list[1][0][0]} {final_list[1][1][0]} {final_list[1][2][0]} |\n'
+          f'| {final_list[0][0][0]} {final_list[0][1][0]} {final_list[0][2][0]} |\n'
           f'---------')
 
 
 if __name__ == "__main__":
     game_end = False
     current_player = 'X'
-    final_array = []
+    final_list = []
     for _ in range(3):
         outer_array = []
         for _ in range(3):
@@ -115,28 +143,28 @@ if __name__ == "__main__":
             inner_array.append(' ')
             inner_array.append(False)
             outer_array.append(inner_array)
-        final_array.append(outer_array)
+        final_list.append(outer_array)
 
-    final_array = final_array[::-1]
+    final_list = final_list[::-1]
 
     # Print the initial table -> start of the games
-    print_table(final_array)
+    print_table(final_list)
 
     while not game_end:
         # Input coordinates
-        enter_coordinates(final_array)
+        enter_coordinates(final_list)
 
         # Print player input
-        print_table(final_array)
+        print_table(final_list)
 
         # Check game status
-        game_state(final_array, current_player)
+        game_state(final_list, current_player)
 
         # Enter computer's coordinates
-        comp_coordinate(final_array, current_player)
+        comp_coordinate(final_list, current_player)
 
         # print computer input
-        print_table(final_array)
+        print_table(final_list)
 
         # Check game status
-        game_state(final_array, current_player)
+        game_state(final_list, current_player)
